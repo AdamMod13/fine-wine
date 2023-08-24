990c8ad8eba8246e4db3f595bd9bb1d01144dc61
Modlich Adam <adam.modlich13@gmail.com>
2023-08-24 20:47:55 +0200

    FW-2: Changed carousel and modified navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
+import {NavbarComponent} from './main-page/navbar/navbar.component';
 import {NgHeroiconsModule} from "@dimaslz/ng-heroicons";
-import {WineCarouselComponent} from './wine-carousel/wine-carousel.component';
+import {WineCarouselComponent} from './main-page/wine-carousel/wine-carousel.component';
 import {NgOptimizedImage} from "@angular/common";
+import {MainPageComponent} from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    WineCarouselComponent
+    WineCarouselComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    backgroundImage: {
+      winery1: "url('./assets/Images/winery_1.jpg')",
+      winery2: "url('./assets/Images/winery_2.jpg')",
+      winery3: "url('./assets/Images/winery_3.jpg')",
+      winery4: "url('./assets/Images/winery_4.jpg')",
+      winery5: "url('./assets/Images/winery_5.jpg')",
+    },
     extend: {
       colors: {
         'main': '#5B0016',
@@ -15,7 +22,8 @@ module.exports = {
     },
     fontFamily: {
       title: ['"Playfair Display"', 'sans-serif'],
-      main: ['"Cormorant Garamond"', 'sans-serif']
+      main: ['"Cormorant Garamond"', 'sans-serif'],
+      quote: ['Qwigley', 'sans-serif']
     }
   },
   plugins: [
