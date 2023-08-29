6eb7af0c445dbc95af2817b9829f190830cb43fb
Modlich Adam <adam.modlich13@gmail.com>
2023-08-29 23:11:05 +0200

    FW-3: Created next part of main page with another header. Fixed sticky navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,19 @@ module.exports = {
       winery3: "url('./assets/Images/winery_3.jpg')",
       winery4: "url('./assets/Images/winery_4.jpg')",
       winery5: "url('./assets/Images/winery_5.jpg')",
-      pouringWine: "url('./assets/Images/pouring_wine.png')"
+      pouringWine: "url('./assets/Images/pouring_wine.png')",
+      wineBottles: "url('./assets/Images/wine_bottles.jpg')",
+      wineRack: "url('./assets/Images/wine_rack.jpg')"
     },
     extend: {
       colors: {
         'main': '#5C001E',
+        'main-light': '#B76E79',
         'light-bg': '#eae8e8',
         'dark-bg': '#1b1b1b',
         'dark': '#262120',
+        'creme': '#f0ece3',
+        'medium': '#b0adad',
         'light': '#fcfcfc'
       },
     },
